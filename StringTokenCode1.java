11568e21d4251298ab08f22f9b1c5fcafff8fb91
GK Sir <hellofcode@hell.you>
2021-05-06 17:01:19 -1100

    KeepCoding :)

--- StringTokenCode1.java
+++ StringTokenCode1.java
@@ -12,17 +12,12 @@ class StringTokenCode {
 
 		System.out.println("data : "+data);
 
-		StringTokenizer str = new StringTokenizer(data);
-		String s1 = str.nextToken();
-		String s2 = str.nextToken();
-		String s3 = str.nextToken();
-		String s4 = str.nextToken();
-
-		System.out.println("s1 : "+s1);
-		System.out.println("s2 : "+s2);
-		System.out.println("s3 : "+s3);
-		System.out.println("s4 : "+s4);
-
-
+		StringTokenizer str = new StringTokenizer(data,"+");
+		
+		while(str.hasMoreTokens())
+		{
+			String s = str.nextToken();
+			System.out.println(" => "+s);
+		}
 	}
 }
