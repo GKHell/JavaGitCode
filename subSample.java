ee07d8d379df5b1fd6ef432310d1ee360b646125
GK Sir <hellofcode@hell.you>
2021-06-09 11:25:05 +0530

    Wed09Jun202111:25:05AM+0530

--- subSample.java
+++ subSample.java
@@ -1,5 +1,11 @@
-import Intro.JavaHell.subPkg;
-class me{
+import Intro.JavaHell.subPkg;		
+
+/**
+ * 
+ * <h1 style='color:red;'>hello World</h1>
+ * 
+ * */
+public class subSample{
 	public static void main(String[] args) {
 		
 		subPkg s = new subPkg();
