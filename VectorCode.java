15a3ef02b008c42c5c08fa54c4e0d367de6c7221
GK Sir <hellofcode@hell.you>
2021-06-21 11:59:51 +0530

    Mon21Jun202111:59:51AM+0530

--- VectorCode.java
+++ VectorCode.java
@@ -32,25 +32,39 @@ class VectorCode{
 		{
 			if(marks.get(i)>=33)
 			{
-				System.out.println("index - "+i+"\t"+marks.get(i));
+				System.out.println("index - "+i+"\t"+marks.get(i)); // a[i]
 				marks_pass.add(marks.get(i));
 			}
 			else
 			{
 				System.out.println("\u001B[31mindex - "+i+"\t"+marks.get(i)+"\u001B[00m");
 				marks_fail.add(marks.get(i));
-
 			}
 
 		}
 
-		// Iterator i = marks.iterator();
-		// while(i.hasNext())
-		// {
-		// 	System.out.println("Marks : "+i.next());
-		// }
+		ListIterator j = marks.listIterator();
 
-		System.out.println("----------------------------------");
+		while(j.hasNext())
+		{
+			System.out.println("\tMarks : "+j.next());
+		}
+
+		while(j.hasPrevious())
+		{
+			System.out.println("Marks : "+j.previous());
+		}
+
+		marks.clear();
+
+		System.out.print("Enter marks for searching : ");
+		System.out.println("status "+marks.contains(sc.nextFloat()));
+		System.out.println("total no of marks : "+marks.size());
+		System.out.println(" total no of pass : "+marks_pass.size());
+		System.out.println(" total no of fail : "+marks_fail.size());
+
+
+		System.out.println("\n\n----------------------------------");
 		for(float f:marks_pass)
 		{
 			System.out.println("pass : "+f);
