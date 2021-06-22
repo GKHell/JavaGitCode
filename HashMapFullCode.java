1a57f556f4b75ec0b1f69c8bf7330df5906a7cf6
GK Sir <hellofcode@hell.you>
2021-06-22 11:44:39 +0530

    Tue22Jun202111:44:39AM+0530

--- HashMapFullCode.java
+++ HashMapFullCode.java
@@ -2,7 +2,7 @@
 import java.util.*;
 import java.io.*;
 class HashMapFullCode{
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		HashMap<Integer,String> data = new HashMap<Integer,String>();
@@ -13,7 +13,7 @@ class HashMapFullCode{
 
 		for(int i=0;i<n;i++)
 		{
-			System.out.println("\nData of STudent ",i+1);
+			System.out.println("\nData of STudent "+(i+1));
 			System.out.print("Enter rollno : ");
 			int rollno = Integer.parseInt(br.readLine());
 
@@ -25,9 +25,30 @@ class HashMapFullCode{
 		}
 
 
+		Set roll_no = data.keySet(); // getting all keys
+		Iterator i = roll_no.iterator();
+		System.out.println("\n-----------------------\n");
+		while(i.hasNext())
+		{
+			Object r =i.next();
+			System.out.println("rollno : "+r+"\t name : "+data.get(r));
+		}
 
-		System.out.println("\n\n\n-------------------");
-		System.out.println(data)
+		// System.out.println("\n\n\n-------------------");
+		// System.out.println(data);
+
+		for(String name:data.values()) // getting all values...
+		{
+			System.out.println("\nname  : "+name);
+		}
+
+
+		/////////////////////// 
+
+		System.out.println("data 1 : "+data.isEmpty());
+		data.clear();
+		System.out.println("data 2 : "+data.isEmpty());
 
 	}
+
 }
