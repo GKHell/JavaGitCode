cc26cbe4bb5e64400a65ab7132e816646e4460ed
GK Sir <hellofcode@hell.you>
2021-06-24 11:16:14 +0530

    Thu24Jun202111:16:14AM+0530

--- StringTokenCode2.java
+++ StringTokenCode2.java
@@ -3,7 +3,7 @@ import java.util.*;
 class StringTokenCode2{
 	public static void main(String[] args) {
 		
-		String s = "I hate programming bcz its just illusion of eyes.";
+		String s = "I hate programming bcz its just an illusion of eyes.";
 
 		// s = "12345x678x90";
 		StringTokenizer data = new StringTokenizer(s,"j");
