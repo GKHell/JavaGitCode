6556d60856ca64f712f472415fe032091a4fbc49
GK Sir <hellofcode@hell.you>
2021-07-05 11:57:51 +0530

    Mon05Jul202111:57:51AM+0530

--- FileClassCode.java
+++ FileClassCode.java
@@ -7,62 +7,63 @@ class FileClassCode{
 
 		File file = new File("code.cpp"); //code.cpp is your file name
 
-		// System.out.println("     isFile : "+file.isFile());
-		// System.out.println("isDirectory : "+file.isDirectory());
-		// System.out.println("    canRead : "+file.canRead());
-		// System.out.println("   canWrite : "+file.canWrite());
-		// System.out.println(" canExecute : "+file.canExecute());
-		// System.out.println("     exists : "+file.exists());
-	    // System.out.println("  getParent : "+file.getParent());
-	    // System.out.println("    getPath : "+file.getPath());
-	    // System.out.println("getAbsolutePath(): "+file.getAbsolutePath());
-	    // System.out.println("       size : "+file.length());
+		System.out.println("     isFile : "+file.isFile());
+		System.out.println("isDirectory : "+file.isDirectory());
+		System.out.println("    canRead : "+file.canRead());
+		System.out.println("   canWrite : "+file.canWrite());
+		System.out.println(" canExecute : "+file.canExecute());
+		System.out.println("     exists : "+file.exists());
+	    System.out.println("  getParent : "+file.getParent());
+	    System.out.println("    getPath : "+file.getPath());
+	    System.out.println("getAbsolutePath(): "+file.getAbsolutePath());
+	    System.out.println("       size : "+file.length());
 
 		System.out.println("\n-----------------------------------------------\n");
-		// System.out.print("Enter the file name you want to delete : ");
-		// String fileName = br.readLine();
-		// file = new File(fileName);
+		System.out.print("Enter the file name you want to delete : ");
+		String fileName = br.readLine();
+		file = new File(fileName);
 
-		// if(file.exists())
-		// {
-		// 	file.delete();
-		// 	System.out.println(fileName+" has been deleted....");
-		// }
-		// else
-		// {
-		// 	System.out.println(fileName+" not exists...");
-		// 	System.out.println("creating.....  "+fileName+" ........");
+		if(file.exists())
+		{
+			file.delete();
+			System.out.println(fileName+" has been deleted....");
+		}
+		else
+		{
+			System.out.println(fileName+" not exists...");
+			System.out.println("creating.....  "+fileName+" ........");
 
 
-		// 	file.createNewFile();
-		// 	System.out.println("Enter 1 to delete.....");
-		// 	int n = Integer.parseInt(br.readLine()); // wait for enter 
+			file.createNewFile();
+			System.out.println("Enter 1 to delete.....");
+			int n = Integer.parseInt(br.readLine()); // wait for enter 
 			
-		// 	if(n==1)
-		// 	{
-		// 		System.out.println(fileName+" has been.. ");
-		// 		file.delete();
-		// 		System.out.println(fileName+" has been deleted....");
-		// 	}
-		// }
+			if(n==1)
+			{
+				System.out.println(fileName+" has been.. ");
+				file.delete();
+				System.out.println(fileName+" has been deleted....");
+			}
+		}
 
 		System.out.println("\n-----------------------------------------------\n");
 
-		// System.out.print("Enter directory name to create : ");
-		// file = new File(br.readLine());
-		// file.mkdir();
-		// System.out.print("directory has been created....");
+		System.out.print("Enter directory name to create : ");
+		file = new File(br.readLine());
+		file.mkdir();
+		System.out.print("directory has been created....");
 
-		// System.out.print("Enter the file Name  : ");
-		// file = new File(br.readLine());
-		// System.out.print("Enter new Name for file : ");
-		// String newName = br.readLine();
-		// file.renameTo(new File(newName));
+		System.out.print("Enter the file Name  : ");
+		file = new File(br.readLine());
+		System.out.print("Enter new Name for file : ");
+		String newName = br.readLine();
+		file.renameTo(new File(newName));
 
 		System.out.println("\n-----------------------------------------------\n");
  		
  		System.out.print("Enter the directory : ");
- 		file = new File(br.readLine());
+ 		String dir = br.readLine();
+ 		file = new File(dir);
  		if(file.exists())
  		{
  			String files[] = file.list();
@@ -70,7 +71,16 @@ class FileClassCode{
  			System.out.println("subfiles..... : ");
  			for(String f : files)
  			{
- 				System.out.println(f);
+ 				System.out.print(f);
+ 				File s = new File(dir+"/"+f);
+ 				if(s.isFile())
+ 				{
+ 					System.out.println(" is a file...");
+ 				}
+ 				else
+ 				{
+ 					System.out.println(" is a directory...");
+ 				}
  			} 
  		}
 
