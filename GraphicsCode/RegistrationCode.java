eb6259dd0d871bca74c1f7d8777732142edc3df1
GK Sir <hellofcode@hell.you>
2021-07-23 16:56:12 +0530

    Fri23Jul202104:56:12PM+0530

--- GraphicsCode/RegistrationCode.java
+++ GraphicsCode/RegistrationCode.java
@@ -12,12 +12,13 @@ class Code{
 		r.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
-class Root extends JFrame implements ActionListener{
+class Root extends JFrame implements ActionListener,ItemListener{
 
 	JPanel mainPanel,formPanel;
-	JLabel lblName,lblRollNo;
+	JLabel lblName,lblRollNo,lblGender; // label
 	JTextField txtName,txtRollNo;
-	JButton btnSave;
+	JRadioButton rbMale,rbFemale,rbOther; //radio Button
+	JButton btnSave; 
 	public Root() //constructor....
 	{
 		mainPanel =  new JPanel();
@@ -37,14 +38,17 @@ class Root extends JFrame implements ActionListener{
 
 		gbc.gridx = 0;
 		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.EAST;
 		lblName = new JLabel("Name : ");
 		formPanel.add(lblName,gbc);
 
 			gbc.gridx = 1;
 			gbc.gridy = 0;
+			gbc.anchor = GridBagConstraints.WEST;
 			txtName = new JTextField(10);
 			formPanel.add(txtName,gbc);
 
+		gbc.anchor = GridBagConstraints.EAST;
 		gbc.gridx = 0;
 		gbc.gridy = 1;
 		lblRollNo = new JLabel("RollNo : ");
@@ -52,8 +56,39 @@ class Root extends JFrame implements ActionListener{
 
 			gbc.gridx = 1;
 			gbc.gridy = 1;
+			gbc.anchor = GridBagConstraints.WEST;
 			txtRollNo = new JTextField(10);
 			formPanel.add(txtRollNo,gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.anchor = GridBagConstraints.EAST;
+		lblGender = new JLabel("Gender : ");
+		formPanel.add(lblGender,gbc);
+
+				gbc.gridx = 1;
+				gbc.gridy = 2; 
+				gbc.anchor = GridBagConstraints.WEST;
+				JPanel p = new JPanel(new FlowLayout());
+				formPanel.add(p,gbc);
+
+				rbMale = new JRadioButton("Male");
+				rbFemale = new JRadioButton("Female");
+				rbOther = new JRadioButton("Other");
+				p.add(rbMale);
+				p.add(rbFemale);
+				p.add(rbOther);
+
+				ButtonGroup grp = new ButtonGroup();
+				grp.add(rbMale);
+				grp.add(rbFemale);
+				grp.add(rbOther);
+
+				rbMale.addItemListener(this);
+				rbFemale.addItemListener(this);
+				rbOther.addItemListener(this);
+
+
 
                // str  => int
 			//    "56" => 56
@@ -83,5 +118,25 @@ class Root extends JFrame implements ActionListener{
 		 		System.out.println("Enter number only.....");
 		 	}
 		 }
+	}
+	int i=0;
+	public void itemStateChanged(ItemEvent ie)
+	{
+		// System.out.println("Hello ---"+(i++));
+		if(ie.getSource()==rbMale)
+		{
+			if(ie.getStateChange()==1)
+				System.out.println(rbMale.getText()+ " selected...");
+		}
+		if(ie.getSource()==rbFemale)
+		{
+			if(ie.getStateChange()==1)
+				System.out.println("\t\t"+rbFemale.getText()+ " selected...");
+		}
+		if(ie.getSource()==rbOther)
+		{
+			if(ie.getStateChange()==1)
+				System.out.println("\t\t\t"+rbOther.getText()+ " selected...");
+		}
 	}
 }
